1735626c84b44c16c5b3f5d2f00b3bb6b9dc3b6d
Benjamin Ramsay <bramsay3@jhu.edu>
2018-07-30 12:31:21 -0400

    crafted JSON insertion query

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
-from psql_json.py import PSQL_Manager
+from psql_json import PSQL_Manager
+import json
 
 input_json = open('orders/2017-10-30.json').read()
+clean_json = json.dumps(json.loads(input_json))
 man = PSQL_Manager()
 
--- psql_json.py
+++ psql_json.py
@@ -21,27 +21,31 @@ class PSQL_Manager:
                 )
         except:
             print("Unable to connect to database")
-        cur = conn.cursor()
+        self.cur = conn.cursor()
 
     def populate_orders(self, json_order):
         self.create_order_table()
-        self.insert_json(json_order)
+        self.insert_order_json(json_order)
 
     def create_order_table(self):
-        create_orders_query = ('CREATE TABLE IF NOT EXISTS orders ('
-                                'order_id integer,'
-                                'order_json json'
-                                ');')
-        cur.execute(create_orders_query)
+        create_orders_query = ("CREATE TABLE IF NOT EXISTS orders ("
+                                "order_id integer,"
+                                "order_json json"
+                                ");")
+        self.cur.execute(create_orders_query)
 
-    def insert_json(self, json):
+    def insert_order_json(self, json):
         """
         Assumes json comes in format of key "orders" followed by
-        an array of order information
+        an array of order information.
         """
 
-        insert_query = ()
+        insert_order_query = ("WITH all_orders (order_data) AS (SELECT * FROM json_array_elements(%s::json->'orders'))"
+                        "INSERT into orders (order_id, order_json VALUES"
+                        "(SELECT * FROM all_orders),"
+                        "(SELECT order_data->'id' FROM all_orders))")
 
+        self.cur.execute(insert_order_query, (json,)
 
 
 
